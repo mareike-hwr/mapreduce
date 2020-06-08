d384c99ff09f2e6f735c159e676ceed3cfa6fdd6
mareike-hwr <s_vogelm19@stud.hwr-berlin.de>
2020-06-09 00:51:13 +0200

    Exercise 2

--- reducer.py
+++ reducer.py
@@ -21,11 +21,12 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 sum_of_values = 0
+count_of_values = 0
 
 # Previous key is initialized with None, we just started
 previous_key = None
 
-# For each new line in the standard input 
+# For each new line in the standard input
 for line in sys.stdin:
 
     # split the line at the tabulator ("\t")
@@ -36,7 +37,7 @@ for line in sys.stdin:
     # Store the 2 elements of this line in seperate variables
     key, value = data
 
-    # Do we have a previous_key (previous_key != None) and 
+    # Do we have a previous_key (previous_key != None) and
     # is the new key different than the previous key?
     # This means the line starts with a new key (key changes e.g. from "Visa" to "Cash")
     # Remember that our keys are sorted
@@ -45,17 +46,19 @@ for line in sys.stdin:
         # to the standart output (stdout)
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+        sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
         # Sum of sales starts again with 0
         sum_of_values = 0
+	count_of_values = 0
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
     sum_of_values += float(value)
+    count_of_values += 1
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
